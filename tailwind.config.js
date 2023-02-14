1416e0158c65d7ec140275887c3abb331c0a215c
kevoff <suviacruzfer@gmail.com>
2023-02-14 19:58:43 -0300

    pre-carga

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+        "orange-x": "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+  plugins: [],
+};
